b4273e1ba77590f6e23096906ecf0f1b6af29c85
youcantry <liu15836533565@163.com>
2024-04-14 16:06:11 +0800

    刘青杭

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,487 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" style="    height: 300px;
+    overflow: scroll;">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <div>
+        <div class="table">
+          <div class="table-wrap">
+            <table border="0" cellpadding="0" cellspacing="0" :style="{width: 3* 100 + 50 + 'px'}">
+              <thead>
+                <tr>
+                  <th>债券名称1</th>
+                  <th>报告名称</th>
+                  <th>剩余期限(年)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="liudehua">刘德华</div>
+                    12121
+                  </td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="table">
+          <div class="table-wrap">
+            <table border="0" cellpadding="0" cellspacing="0" :style="{width: 3* 100 + 50 + 'px'}">
+              <thead>
+                <tr>
+                  <th>债券名称2</th>
+                  <th>报告名称</th>
+                  <th>剩余期限(年)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="liudehua">刘德华</div>
+                    12121
+                  </td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="table">
+          <div class="table-wrap">
+            <table border="0" cellpadding="0" cellspacing="0" :style="{width: 3* 100 + 50 + 'px'}">
+              <thead>
+                <tr>
+                  <th>债券名称3</th>
+                  <th>报告名称</th>
+                  <th>剩余期限(年)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="liudehua">刘德华</div>
+                    12121
+                  </td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="table">
+          <div class="table-wrap">
+            <table border="0" cellpadding="0" cellspacing="0" :style="{width: 3* 100 + 50 + 'px'}">
+              <thead>
+                <tr>
+                  <th>债券名称4</th>
+                  <th>报告名称</th>
+                  <th>剩余期限(年)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="liudehua">刘德华</div>
+                    12121
+                  </td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="table">
+          <div class="table-wrap">
+            <table border="0" cellpadding="0" cellspacing="0" :style="{width: 3* 100 + 50 + 'px'}">
+              <thead>
+                <tr>
+                  <th>债券名称5</th>
+                  <th>报告名称</th>
+                  <th>剩余期限(年)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="liudehua">刘德华</div>
+                    12121
+                  </td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+                <tr>
+                  <td>12121</td>
+                  <td>12121</td>
+                  <td>12122</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +493,105 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      a: 1
+    }
+  },
+  mounted () {
+    const element = document.getElementsByClassName('home')
+    const table = document.getElementsByClassName('table-wrap')
+    const liudehua = document.getElementsByClassName('liudehua')
+    element[0].addEventListener('scroll', function () {
+    //   console.log('123')
+    //   console.log('123element', element)
+      console.log('element', element[0].scrollTop)
+      for (let i = 0; i < liudehua.length; i++) {
+        if (liudehua[i].getBoundingClientRect().y < 120) {
+        //   console.log('123')
+          liudehua[i].innerText = '梁朝伟'
+          liudehua[i].onclick = function () {
+            element[0].scrollTop = element[0].scrollTop - 100
+          }
+        } else if (liudehua[i].getBoundingClientRect().y > 120) {
+          liudehua[i].innerText = '周星驰'
+          liudehua[i].onclick = function () {
+            element[0].scrollTop = element[0].scrollTop - 100
+          }
+        }
+      }
+      console.log('tablegetBoundingClientRect', table[0].getBoundingClientRect())
+      console.log('tablegetBoundingClientRect1', table[1].getBoundingClientRect())
+      console.log('tablegetBoundingClientRect2', table[2].getBoundingClientRect())
+      console.log('tablegetBoundingClientRect3', table[3].getBoundingClientRect())
+    //   const scrollDistance = table[0].scrollHeight - table[0].scrollTop - table[0].clientHeight
+    //   console.log('scrollDistance', scrollDistance)
+    })
+    // console.log('elemtnt', element[0].children[2].children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[0])
   }
 }
 </script>
+<style scoped lang="stylus">
+.title
+  padding 5px 15px
+
+  /deep/.xn-card__head.van-hairline--bottom
+    .xn-card__title
+      font-size 32px
+      font-weight bold
+
+  .content
+    .smallTitle
+      font-weight bold
+
+    .detailGrid
+      /deep/.xn-detail-grid.xn-detail-grid--row
+        display flex
+        align-items baseline
+        padding 10px 0
+
+    .table-wrap
+      overflow-x auto
+      background #fff
+
+      thead
+        background #f8f8f8
+
+        th
+          min-width 80px
+          height 80px
+          border-top 1px solid #e1e1e1
+          border-right 1px solid #e1e1e1
+          white-space nowrap
+          padding 5px
+
+        th:first-child
+          border-left 1px solid #e1e1e1
+
+      tr:first-child
+        td
+          border-top 1px solid #e1e1e1
+
+      tr
+        td
+          margin 0
+          border 0
+          padding 14px 24px
+          border-bottom 1px solid #e1e1e1
+          border-right 1px solid #e1e1e1
+          text-align center
+          font-size 24px
+          color #606266
+
+        td:first-child
+          padding 20px 32px
+          border-left 1px solid #e1e1e1
+
+        td:last-child
+          border-right 1px solid #e1e1e1
+
+.sanjiao
+  width 50px
+  height 50px
+</style>
